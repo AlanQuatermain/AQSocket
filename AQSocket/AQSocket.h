c0b720ae9a6c00eeccefb165bda83790792d3317
Jim Dovey <jimdovey@mac.com>
2011-12-16 13:51:36 -0500

    Initial Commit

---
+++ AQSocket/AQSocket.h
@@ -0,0 +1,13 @@
+//
+//  AQSocket.h
+//  AQSocket
+//
+//  Created by Jim Dovey on 11-12-16.
+//  Copyright (c) 2011 Kobo Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AQSocket : NSObject
+
+@end
---
+++ AQSocket/AQSocket.m
@@ -0,0 +1,23 @@
+//
+//  AQSocket.m
+//  AQSocket
+//
+//  Created by Jim Dovey on 11-12-16.
+//  Copyright (c) 2011 Kobo Inc. All rights reserved.
+//
+
+#import "AQSocket.h"
+
+@implementation AQSocket
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
